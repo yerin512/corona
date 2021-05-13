9b6802cc348bedad2c7443a04a707314fe40a7ce
yerin512 <sosu0512@naver.com>
2021-05-13 15:22:12 +0900

    bar

---
+++ src/components/Contents.js
@@ -0,0 +1,60 @@
+import { useState, useEffect } from 'react';
+import { Bar, Doughnut, Line } from 'react-chartjs-2';
+import axios from 'axios';
+
+const Contents = () => {
+
+    const [confirmedData, setConfirmedData] = useState({
+        labels: ["1월","2월","3월"],
+        datasets:[
+            {
+                label:"국내 누적 확진자",
+                backgroundColor:"salmon",
+                fill:true,
+                data:[10, 5, 3]
+            },
+        ]
+
+    })
+
+    useEffect(() => {
+        const fetchEvents = async () => {
+            const res = await axios.get("https://api.covid19api.com/total/dayone/country/kr")
+            makeData(res.data)
+        }
+        const makeData = (items) => {
+            const arr = items.reduce((acc, cur) => {
+                const currentDate = new Date(cur.Date);
+                const year = currentDate.getFullYear();
+                const month = currentDate.getMonth();
+                const date = currentDate.getDate();
+                const confirmed = cur.confirmed;
+                const active = cur.Active;
+                const death = cur.Deaths;
+                const recovered = cur.Recovered;
+
+                return acc;
+
+            })
+        }
+
+        fetchEvents();
+    })
+
+    return (
+        <section>
+            <h2>국내 코로나 현황</h2>
+            <div className="contents">
+                <div>
+                    <Bar data={confirmedData} options={
+                        { title: { display: true, text: "누적 확진자 추이", fontSize: '16px' } },
+                        { legend: { display: true, position: "bottom" } }
+                    }
+                    />
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Contents
