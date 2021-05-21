fbfb4b7a3f723ce52c8da1dbac2c03193e72dee7
yerin512 <sosu0512@naver.com>
2021-05-21 15:00:29 +0900

    33

--- src/components/Contents.js
+++ src/components/Contents.js
@@ -4,103 +4,103 @@ import axios from 'axios';
 
 const Contents = () => {
 
-    const [confirmedData , setConfirmedData] = useState({})
-    const [quarantinedData , setQuarantinedData] = useState({})
-    const [comparedData, setCompatedData] = useState({}) 
+    // const [confirmedData , setConfirmedData] = useState({})
+    // const [quarantinedData , setQuarantinedData] = useState({})
+    // const [comparedData, setCompatedData] = useState({}) 
 
-    useEffect(() => {
-        const fetchEvents = async () => {
-            const res = await axios.get("https://api.covid19api.com/total/dayone/country/kr")
-            makeData(res.data)
-            // console.log(res);
+    // useEffect(() => {
+    //     const fetchEvents = async () => {
+    //         const res = await axios.get("https://api.covid19api.com/total/dayone/country/kr")
+    //         makeData(res.data)
+    //         // console.log(res);
 
-            const summary = await axios.get('https://api.covid19api.com/total/dayone/country/kr');
+    //         // const summary = await axios.get('https://api.covid19api.com/total/dayone/country/kr');
 
-            console.log(summary);
-        }
-        const makeData = (items) => {
-            const arr = items.reduce((acc, cur) => {               
-                // console.log(cur)
-                const currentDate = new Date(cur.Date);
-                const year = currentDate.getFullYear();
-                const month = currentDate.getMonth();
-                const date = currentDate.getDate();
-                const confirmed = cur.Confirmed;
-                const active = cur.Active;
-                const death = cur.Deaths;
-                const recovered = cur.Recovered;
+    //         console.log(summary);
+    //     }
+    //     const makeData = (items) => {
+    //         const arr = items.reduce((acc, cur) => {               
+    //             // console.log(cur)
+    //             const currentDate = new Date(cur.Date);
+    //             const year = currentDate.getFullYear();
+    //             const month = currentDate.getMonth();
+    //             const date = currentDate.getDate();
+    //             const confirmed = cur.Confirmed;
+    //             const active = cur.Active;
+    //             const death = cur.Deaths;
+    //             const recovered = cur.Recovered;
 
-                const findItem = acc.find(a=> a.year === year && a.year === year && a.month === month);
+    //             const findItem = acc.find(a=> a.year === year && a.year === year && a.month === month);
 
-                if(!findItem){
-                    acc.push({year, month, date, confirmed, active, death, recovered})
-                }
+    //             if(!findItem){
+    //                 acc.push({year, month, date, confirmed, active, death, recovered})
+    //             }
 
-                if(findItem && findItem.date < date){
-                    findItem.active = active;
-                    findItem.death = death;
-                    findItem.date = date;
-                    findItem.year = year;
-                    findItem.recovered = recovered;
-                    findItem.confirmed = confirmed;
-                }
-                return acc;
+    //             if(findItem && findItem.date < date){
+    //                 findItem.active = active;
+    //                 findItem.death = death;
+    //                 findItem.date = date;
+    //                 findItem.year = year;
+    //                 findItem.recovered = recovered;
+    //                 findItem.confirmed = confirmed;
+    //             }
+    //             return acc;
 
-            }, [])
+    //         }, [])
             
-            const labels = arr.map(a=> `${a.month + 1}월`);
-            setConfirmedData({
-                labels,
-                datasets:[
-                    {
-                        label:"국내 누적 확진자",
-                        backgroundColor:"salmon",
-                        fill: true,
-                        data: arr.map(a=>a.confirmed)
-                    },
-                ]
-            })
+    //         const labels = arr.map(a=> `${a.month + 1}월`);
+    //         setConfirmedData({
+    //             labels,
+    //             datasets:[
+    //                 {
+    //                     label:"국내 누적 확진자",
+    //                     backgroundColor:"salmon",
+    //                     fill: true,
+    //                     data: arr.map(a=>a.confirmed)
+    //                 },
+    //             ]
+    //         })
 
-            setQuarantinedData({
-                labels,
-                datasets:[
-                    {
-                        label:"월별 격리자 현황",
-                        borderColor:"salmon",
-                        fill: false,
-                        data: arr.map(a=>a.confirmed)
-                    },
-                ]
-            })
+    //         setQuarantinedData({
+    //             labels,
+    //             datasets:[
+    //                 {
+    //                     label:"월별 격리자 현황",
+    //                     borderColor:"salmon",
+    //                     fill: false,
+    //                     data: arr.map(a=>a.confirmed)
+    //                 },
+    //             ]
+    //         })
 
-            const last = arr[arr.length - 1];
+    //         const last = arr[arr.length - 1];
 
-            setCompatedData({
-                labels:["확진자", "격리해제", "사망"],
-                datasets:[
-                    {
-                        label:"누적 확진, 해제, 사망 비율",
-                        backgroundColor:["#ff3d67", "#059bff", "#ffc233"],
-                        borderColor:["#ff3d67", "#059bff", "#ffc233"],
-                        fill: false,
-                        data: [last.confirmed, last.recovered, last.death]
-                    },
-                ]
-            })
+    //         setCompatedData({
+    //             labels:["확진자", "격리해제", "사망"],
+    //             datasets:[
+    //                 {
+    //                     label:"누적 확진, 해제, 사망 비율",
+    //                     backgroundColor:["#ff3d67", "#059bff", "#ffc233"],
+    //                     borderColor:["#ff3d67", "#059bff", "#ffc233"],
+    //                     fill: false,
+    //                     data: [last.confirmed, last.recovered, last.death]
+    //                 },
+    //             ]
+    //         })
 
 
-        }
+    //     }
         
 
-        fetchEvents();
-    },[])
+    //     fetchEvents();
+    // },[])
 
 
 
     return (
         <section>
             <h2>국내 코로나 현황</h2>
-            <div className="contents">
+            {/* <div className="contents">
                 <div>
                     <Bar data={confirmedData} options={
                         { title: { display: true, text: "누적 확진자 추이", fontSize: '16px' } },
@@ -122,7 +122,7 @@ const Contents = () => {
                     }
                     />
                 </div>
-            </div>
+            </div> */}
         </section>
     )
 }
